c19ee2bf4e46e14888969baff083d1e18fa926ae
anton <kornilov@realweb.ru>
2014-05-29 12:57:29 +0400

    add composer autoloader fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 /* @var Composer\Autoload\ClassLoader $autoloader */
-$autoloader = include '../vendor/autoload.php';
+$autoloader = include __DIR__ . '/../vendor/autoload.php';
 $autoloader->add('Kachit\Helper\Tests', __DIR__);
